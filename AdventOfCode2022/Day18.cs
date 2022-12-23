90dd237b08f1296e9458ae39c48bbc44ec1059e9
Ihazevich <ihazevich@outlook.com>
2022-12-23 13:23:55 +0100

    Day 18 part 2 - Could be faster but it works

--- AdventOfCode2022/Day18.cs
+++ AdventOfCode2022/Day18.cs
@@ -4,28 +4,14 @@ using System.Net.Http.Headers;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using static AdventOfCode2022.Day17;
+using static AdventOfCode2022.Day18;
 
 namespace AdventOfCode2022
 {
     internal class Day18
     {
-        public static bool Verbose = false;
-
-        public static Stopwatch Watch = new();
-
-        public static List<Rock> Rocks = new List<Rock>();
-        public static int MaxHeightP1 = 0;
-
-        public static int[] Jets = new int[0];
-        public static int JetsP1 = 0;
-
-        public const int SIZE = 500_000;
-
         public static Tuple<string, string> Solve(string input)
         {            
-            var t = new Stopwatch();
-            t.Start();
-
             var dropletScan = File.ReadAllLines(input);
             var part1 = "";
             var part2 = "";
@@ -36,9 +22,10 @@ namespace AdventOfCode2022
             {
                 var cube = new DropletScanCube
                 {
-                    Coords = cubeScan.Split(",").Select(s => int.Parse(s)).ToArray()
+                    Coords = cubeScan.Split(",").Select(s => int.Parse(s)).ToArray(),
+                    Material = CubeMaterial.LAVA
                 };
-                droplet.Add(cube);                
+                droplet.AddLava(cube);                
             }
 
             var exposedSides = 0;
@@ -49,6 +36,23 @@ namespace AdventOfCode2022
             }
             part1 = $"{exposedSides}";
 
+            droplet.AddAir();
+            droplet.FillWithWater();
+
+            var totalLavaPockets = droplet.Cubes.Count(c => c.Material == CubeMaterial.LAVA);
+            var totalAirPockets = droplet.Cubes.Count(c => c.Material == CubeMaterial.AIR);
+            var totalWPockets = droplet.Cubes.Count(c => c.Material == CubeMaterial.WATER);
+            var totalPockets = droplet.Cubes.Count();
+
+            droplet.RecalculateExposure();
+
+            exposedSides = 0;
+
+            foreach (var c in droplet.Cubes.Where(c => c.Material == CubeMaterial.LAVA))
+            {
+                exposedSides += 6 - c.Neighbours;
+            }
+            part2 = $"{exposedSides}";
 
 
             return new Tuple<string, string>(part1, part2);
@@ -58,17 +62,26 @@ namespace AdventOfCode2022
         {
             public int[] Coords;
             public int Neighbours;
+            public CubeMaterial Material;
+        }
+
+        public enum CubeMaterial
+        {
+            LAVA,
+            AIR,
+            WATER
         }
 
         public class Droplet
         {
             public List<DropletScanCube> Cubes = new();
 
-            public int ExposedSides = 0;
+            public int[] Max = new int[] { int.MinValue, int.MinValue, int.MinValue };
+            public int[] Min = new int[] { int.MaxValue, int.MaxValue, int.MaxValue };
 
-            public void Add(DropletScanCube cube)
+            public void AddLava(DropletScanCube cube)
             {
-                foreach(var c in Cubes)
+                foreach(var c in Cubes.Where(c => c.Material == CubeMaterial.LAVA))
                 {
                     //var adjacent = new int[3];
                     var total = 0;
@@ -82,7 +95,175 @@ namespace AdventOfCode2022
                         c.Neighbours++;
                     }
                 }
+
+                for(int i = 0; i < 3; i++)
+                {
+                    Max[i] = Math.Max(Max[i], cube.Coords[i]);
+                    Min[i] = Math.Min(Min[i], cube.Coords[i]);
+                }
+
                 Cubes.Add(cube);
+            }
+            
+            public bool HasMaterialAt(int x, int y, int z, CubeMaterial material)
+            {
+                return Cubes.Any(c => c.Material == material && c.Coords[0] == x && c.Coords[1] == y && c.Coords[2] == z);
+            }
+
+            public void AddAir()
+            {                
+                for(var z = Min[2]; z <= Max[2]; z++)
+                {
+                    for (var y = Min[1]; y <= Max[1]; y++)
+                    {
+                        for (var x = Min[0]; x <= Max[0]; x++)
+                        {
+                            if (HasMaterialAt(x, y, z, CubeMaterial.LAVA)) continue;
+                            var airCube = new DropletScanCube
+                            {
+                                Coords = new int[] { x,y,z },
+                                Material = CubeMaterial.AIR
+                            };
+                            Cubes.Add(airCube);
+                        }
+                    }
+                }
+            }
+
+            public void ChangeMaterial(int x, int y, int z, CubeMaterial material)
+            {
+                var index = Cubes.FindIndex(c => c.Coords[0] == x && c.Coords[1] == y && c.Coords[2] == z);
+
+                if (index == -1) throw new Exception();
+
+                Cubes[index].Material = material;
+            }
+
+            public void FillWithWater()
+            {
+                // Round 1
+                for(var z = Min[2]; z <= Max[2]; z++)
+                {
+                    for (var y = Min[1]; y <= Max[1]; y++)
+                    {
+                        for (var x = Min[0]; x <= Max[0]; x++)
+                        {
+                            if (HasMaterialAt(x, y, z, CubeMaterial.LAVA)) continue;
+                            
+                            if (x == Min[0] || x == Max[0]) ChangeMaterial(x, y, z, CubeMaterial.WATER);
+                            if (y == Min[1] || y == Max[1]) ChangeMaterial(x, y, z, CubeMaterial.WATER);
+                            if (z == Min[2] || z == Max[2]) ChangeMaterial(x, y, z, CubeMaterial.WATER);
+
+                            if (HasMaterialAt(x, y, z, CubeMaterial.WATER))
+                            {
+                                for(var nz = z - 1; nz <= z + 1; nz++)
+                                {
+                                    for (var ny = y - 1; ny <= y + 1; ny++)
+                                    {
+                                        for (var nx = x - 1; nx <= x + 1; nx++)
+                                        {
+                                            if ((nz != z && ny != y) || (nz != z && nx != x) || (nx != x && ny != y)) continue;
+
+                                            if (HasMaterialAt(nx, ny, nz, CubeMaterial.AIR))
+                                            {
+                                                ChangeMaterial(nx, ny, nz, CubeMaterial.WATER);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            else if (HasMaterialAt(x, y, z, CubeMaterial.AIR))
+                            {
+                                for (var nz = z - 1; nz <= z + 1; nz++)
+                                {
+                                    for (var ny = y - 1; ny <= y + 1; ny++)
+                                    {
+                                        for (var nx = x - 1; nx <= x + 1; nx++)
+                                        {
+                                            if ((nz != z && ny != y) || (nz != z && nx != x) || (nx != x && ny != y)) continue;
+
+                                            if (HasMaterialAt(nx, ny, nz, CubeMaterial.WATER))
+                                            {
+                                                ChangeMaterial(x, y, z, CubeMaterial.WATER);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Round 2 Reverse
+                for (var z = Max[2]; z >= Min[2]; z--)
+                {
+                    for (var y = Max[1]; y >= Min[1]; y--)
+                    {
+                        for (var x = Max[0]; x >= Min[0]; x--)
+                        {
+                            if (HasMaterialAt(x, y, z, CubeMaterial.LAVA)) continue;
+
+                            if (HasMaterialAt(x, y, z, CubeMaterial.WATER))
+                            {
+                                for (var nz = z - 1; nz <= z + 1; nz++)
+                                {
+                                    for (var ny = y - 1; ny <= y + 1; ny++)
+                                    {
+                                        for (var nx = x - 1; nx <= x + 1; nx++)
+                                        {
+                                            if ((nz != z && ny != y) || (nz != z && nx != x) || (nx != x && ny != y)) continue;
+
+                                            if (HasMaterialAt(nx, ny, nz, CubeMaterial.AIR))
+                                            {
+                                                ChangeMaterial(nx, ny, nz, CubeMaterial.WATER);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            else if (HasMaterialAt(x, y, z, CubeMaterial.AIR))
+                            {
+                                for (var nz = z - 1; nz <= z + 1; nz++)
+                                {
+                                    for (var ny = y - 1; ny <= y + 1; ny++)
+                                    {
+                                        for (var nx = x - 1; nx <= x + 1; nx++)
+                                        {
+                                            if ((nz != z && ny != y) || (nz != z && nx != x) || (nx != x && ny != y)) continue;
+
+                                            if (HasMaterialAt(nx, ny, nz, CubeMaterial.WATER))
+                                            {
+                                                ChangeMaterial(x, y, z, CubeMaterial.WATER);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            public void RecalculateExposure()
+            {
+                foreach (var c1 in Cubes.Where(c => c.Material == CubeMaterial.LAVA))
+                {
+                    foreach (var c2 in Cubes.Where(c => c.Material == CubeMaterial.AIR))
+                    {
+                        var total = 0;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            total += Math.Abs(c1.Coords[i] - c2.Coords[i]);
+                        }
+                        if (total == 1)
+                        {
+                            c1.Neighbours++;
+                            if (c1.Neighbours > 6) throw new Exception();
+                        }
+                    }
+                }
             }
         }
     }
